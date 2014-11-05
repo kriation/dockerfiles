082310086faf1f555bdab1ea40b5c57ad62559b8
Michael Grosser <development@stp-ip.net>
2014-11-05 21:02:45 +0100

    Fix conditional logic

--- mariadb/entrypoint.sh
+++ mariadb/entrypoint.sh
@@ -27,46 +27,45 @@ if [ -a "$CUSTOM_DATA_DIR/DEFAULT" ]; then
     echo >&2 "Please substitute $CUSTOM_DATA_DIR with a volume."
     echo >&2 "Use a Data Volume Container or another method to mount a volume."
     echo >&2 "$delimiter"    
-else
+fi
 
-    if [ -z "$(ls -A $CUSTOM_DATA_DIR)" -a "${1%_safe}" = 'mysqld' ]; then
-        echo >&2 "ERROR: database is uninitialized."
-        if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
-            echo >&2 "No MYSQL_ROOT_PASSWORD was provided."
-            echo >&2 "Add one via a volume and $CUSTOM_CONFIG_DIR/ENV."
-            echo >&2 "Or via -e MYSQL_ROOT_PASSWORD (not recommended)."
-            exit 1
-        fi
+if [ -z "$(ls -A $CUSTOM_DATA_DIR)" -a "${1%_safe}" = 'mysqld' ]; then
+    echo >&2 "ERROR: database is uninitialized."
+    if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
+        echo >&2 "No MYSQL_ROOT_PASSWORD was provided."
+        echo >&2 "Add one via a volume and $CUSTOM_CONFIG_DIR/ENV."
+        echo >&2 "Or via -e MYSQL_ROOT_PASSWORD (not recommended)."
+        exit 1
+    fi
 
-        mysql_install_db --data=$CUSTOM_DATA_DIR
+    mysql_install_db --data=$CUSTOM_DATA_DIR
 
-        # These statements _must_ be on individual lines, and _must_ end with
-        # semicolons (no line breaks or comments are permitted).
-        # TODO proper SQL escaping on ALL the things D:
-        TEMP_FILE='/tmp/mysql-first-time.sql'
-        cat > "$TEMP_FILE" <<-EOSQL
-            DELETE FROM mysql.user ;
-            CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
-            GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
-            DROP DATABASE IF EXISTS test ;
+    # These statements _must_ be on individual lines, and _must_ end with
+    # semicolons (no line breaks or comments are permitted).
+    # TODO proper SQL escaping on ALL the things D:
+    TEMP_FILE='/tmp/mysql-first-time.sql'
+    cat > "$TEMP_FILE" <<-EOSQL
+        DELETE FROM mysql.user ;
+        CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
+        GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
+        DROP DATABASE IF EXISTS test ;
 EOSQL
 
-        if [ "$MYSQL_DATABASE" ]; then
-            echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" >> "$TEMP_FILE"
-        fi
-
-        if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
-            echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> "$TEMP_FILE"
-	
-            if [ "$MYSQL_DATABASE" ]; then
-                echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> "$TEMP_FILE"
-            fi
-        fi
-	
-        echo 'FLUSH PRIVILEGES ;' >> "$TEMP_FILE"
-	
-        set -- "$@" --init-file="$TEMP_FILE"
+    if [ "$MYSQL_DATABASE" ]; then
+        echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" >> "$TEMP_FILE"
     fi
+
+    if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
+        echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> "$TEMP_FILE"
+	
+        if [ "$MYSQL_DATABASE" ]; then
+            echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> "$TEMP_FILE"
+        fi
+    fi
+	
+    echo 'FLUSH PRIVILEGES ;' >> "$TEMP_FILE"
+	
+    set -- "$@" --init-file="$TEMP_FILE"
 fi
 
 # Adjust owner and permissions
