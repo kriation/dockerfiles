76ce14e867ad7613a4030c18e378482ac858e71b
Michael Grosser <development@stp-ip.net>
2014-11-19 14:12:13 +0100

    Conform withproposed standard spec.

--- mariadb/entrypoint.sh
+++ mariadb/entrypoint.sh
@@ -1,33 +1,11 @@
-#!/bin/bash
+#/bin/bash
 set -e
 
-delimiter=">>>"
+./default_check.sh
 
-if [ -a "$CUSTOM_CONFIG_DIR/DEFAULT" ]; then
-    echo >&2 "Default configuration directory in use."
-    echo >&2 "Configurations can be provided via volume at $CUSTOM_CONFIG_DIR."
-    echo >&2 "mysqld configuration will be read from $CUSTOM_CONFIG_DIR/conf.d/*."
-    echo >&2 "ENVs will be read from $CUSTOM_CONFIG_DIR/ENV."
-    echo >&2 "$delimiter"
-else
-    $CUSTOM_CONFIG_DIR/ENV
-fi
-
-if [ -a "$CUSTOM_SECRETS_DIR/DEFAULT" ]; then
-    echo >&2 "Default secret directory in use."
-    echo >&2 "Keys/secrets can be provided via volume at $CUSTOM_SECRETS_DIR."
-    echo >&2 "Secure ENVs will be read from $CUSTOM_SECRETS_DIR/ENV."
-    echo >&2 "$delimiter"
-else
-    $CUSTOM_SECRETS_DIR/ENV
-fi
-
-if [ -a "$CUSTOM_DATA_DIR/DEFAULT" ]; then
-    echo >&2 "WARNING: Non persistent data directory in use! Data loss on restart!"
-    echo >&2 "Please substitute $CUSTOM_DATA_DIR with a volume."
-    echo >&2 "Use a Data Volume Container or another method to mount a volume."
-    echo >&2 "$delimiter"    
-fi
+# Establish ENVs
+./$CUSTOM_CONFIG_DIR/ENV
+./$CUSTOM_SECRET_DIR/ENV
 
 if [ -z "$(ls -A $CUSTOM_DATA_DIR)" -a "${1%_safe}" = 'mysqld' ]; then
     echo >&2 "ERROR: database is uninitialized."
